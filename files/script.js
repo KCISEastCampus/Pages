d37598d2bbec35a16d27298a5a2b012472b42ef0
Eric Stone <ericstone2009@outlook.com>
2024-11-04 19:04:08 +0800

    IGCSE Index

---
+++ files/script.js
@@ -0,0 +1,11 @@
+function notAbletoAccess() {
+    alert("内容因维护，暂停访问，感谢您的理解与支持！")
+}
+
+function underConstruction() {
+    alert("内容正在建造中，暂时停止访问，感谢您的理解与支持！")
+}
+
+function engUnderConstruction() {
+    alert("The content is under construction and access is temporarily suspended. Thank you for your understanding and support!")
+}
