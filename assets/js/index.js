a43a43b43d097d494a47d1c3459140abf093ca69
idadwind1 <idadwind@gmail.com>
2025-01-27 22:06:23 +0800

    edit css clear css code & change theme color

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+function bind_onclick_btn() {
+  let buttons = document.getElementsByClassName('button-link');
+  for (let button in buttons) {
+
+  }
+}
+function load_lang() {
+
+}
+console.log("index.js loaded");
